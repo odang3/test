0b4b75b2bf9843cbfe5c8a1c0805bbb6c4a30cab
odang3 <showme1470@gmail.com>
2022-07-12 08:32:30 +0900

    interface

---
+++ src/main/java/com/example/test/Test2.java
@@ -0,0 +1,9 @@
+package com.example.test;
+
+public interface Test2 {
+
+
+
+
+}
+
--- src/main/java/com/example/test/TestDto.java
+++ src/main/java/com/example/test/TestDto.java
@@ -1,7 +1,15 @@
 package com.example.test;
 
-public class TestDto {
+public class TestDto implements Test2 {
 
     private String name2;
+
+    TestDto(){
+        super();
+    }
+    TestDto(String name2){
+        this();
+        this.name2 = name2;
+    }
 
 }
